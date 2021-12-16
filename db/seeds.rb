42b38c2bd3cfca7b71f3f8b31e4c96fc5457b5e8
Antonin Folliasson <antonin@aitio.co>
2021-12-16 20:29:16 +0100

    en dev début

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Flat.destroy_all
+
+(1..5).each do |my_flat|
+  Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: (75..234).to_a.sample,
+    number_of_guests: (1..8).to_a.sample
+  )
+end
